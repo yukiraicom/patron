8b4ac52f2d28b59a8f65ba3fdbce599178297dda
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-04 12:37:23 +0900

    tweetモジュール

---
+++ lib/twieet.rb
@@ -0,0 +1,14 @@
+module Notice
+  def tweet
+    require 'twitter'
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['consumer_key']
+      config.consumer_secret = ENV['consumer_secret']
+      config.access_token = ENV['access_token']
+      config.access_token_secret = ENV['access_token_secret']
+    end
+
+    client.update("test")
+  end
+end

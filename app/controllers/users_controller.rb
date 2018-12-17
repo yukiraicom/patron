486cc1ead64fe8c972a264efdfe321f87183a844
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-18 00:12:47 +0900

    user検索

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
   end
   def search
-    
+    user_name = params.require(:user).permit(:name)[:name]
+    @users = User.where('username LIKE ?',"%#{user_name}%")
   end
 end

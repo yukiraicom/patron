034ac43ec048af74fd7ac1c7855474361ccacf6c
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-18 11:58:20 +0900

    ユーザーページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def search
     user_name = params.require(:user).permit(:name)[:name]
     @users = User.where('username LIKE ?',"%#{user_name}%")

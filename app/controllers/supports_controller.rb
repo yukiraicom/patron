d802a89089bc7e9dfc1fe60f42daa37f466d2347
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-18 12:18:20 +0900

    support table にテータ入れる昨日

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,2 +1,6 @@
 class SupportsController < ApplicationController
+  def create
+    @support = params.permit(:format)[:format]
+    Support.create(patreon_id: current_user.id, creater_id: @support)
+  end
 end

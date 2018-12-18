d7761c7809751c35e99051e66c22fb57121743fa
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-18 12:28:23 +0900

    サポート済みかの真偽

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @support = Support.new
+    @is_supported = Support.exists?(patreon_id: current_user.id, creater_id: @user)
   end
 
   def search

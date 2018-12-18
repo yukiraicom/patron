cf6b8028dfda670848bd6176ceb409dc92cba480
yukiraicom <yuukiarai19970524@gmail.com>
2018-12-18 13:47:35 +0900

    サポート後リダイレクト

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -2,5 +2,8 @@ class SupportsController < ApplicationController
   def create
     @support = params.permit(:format)[:format]
     Support.create(patreon_id: current_user.id, creater_id: @support)
+    redirect_to user_path(@support)
+  end
+  def destroy
   end
 end
